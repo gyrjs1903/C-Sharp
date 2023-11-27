acccaf834588a1838f88aaff088984c29e951598
gyrjs1903 <gyrjs1903@gmail.com>
2023-11-27 13:29:57 +0900

    C#시작

---
+++ MainApp.cs
@@ -0,0 +1,13 @@
+﻿namespace Hello
+{
+    class MainApp
+    {
+        //프로그램 실행이 시작되는 곳
+        static void Main(string[] args)
+        {
+            if (args.Length ==0) {
+                Console.WriteLine("Hello, World!");
+            }
+        }
+    }
+}
